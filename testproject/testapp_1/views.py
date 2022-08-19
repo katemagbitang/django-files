2d03982536b023ae3e08402e280c88f69f6b3baf
katemagbitang <breakatedown@gmail.com>
2022-08-19 16:09:43 +0800

    initial upload code (buggy)

--- testproject/testapp_1/views.py
+++ testproject/testapp_1/views.py
@@ -1,10 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import testModel
+from .forms import UploadFileForm
 
 def index(request):
-    return render(request,'index.html')   
+    if request.method == 'POST':
+        filled_form = UploadFileForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Uploaded'
+            new_form = UploadFileForm()
+            return render(request,'index.html', {'uploadform':new_form, 'note':note})    
+    else:
+        form = UploadFileForm()
+        return render(request,'index.html', {'uploadform':form})   
 
 def upload(request):
     testFiles = testModel.objects.all()
