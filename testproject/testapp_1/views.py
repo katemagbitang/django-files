ad26e3cdede6b6b0760f24fe5c17ba63e8f318ee
katemagbitang <breakatedown@gmail.com>
2022-08-23 17:15:28 +0800

    fixed views #2

--- testproject/testapp_1/views.py
+++ testproject/testapp_1/views.py
@@ -52,4 +52,6 @@ def importFile(request):
             value = fileRead(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
                                 data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20])
             value.save()
-    return render(request, 'import.html')
+        return render(request, 'import.html',{'note':'Imported'})
+    else:
+        return render(request, 'import.html')
