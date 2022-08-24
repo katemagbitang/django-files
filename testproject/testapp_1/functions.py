6df8ff4ede960d0760b6bbd9bfcdc1c80e7466b7
katemagbitang <breakatedown@gmail.com>
2022-08-24 15:39:56 +0800

    BU validation added and cleaned initial code

--- testproject/testapp_1/functions.py
+++ testproject/testapp_1/functions.py
@@ -5,6 +5,7 @@ def handle_uploaded_file(f):
         for chunk in f.chunks():
             destination.write(chunk)
 
+# checks if the email is valid
 def validateEmail(email):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     if(re.fullmatch(regex, email)):
@@ -12,9 +13,34 @@ def validateEmail(email):
     else:
         return 'Invalid Email'
 
-# def numberToString(f):
-#     f = str(f)
-#     return f
+# checks if the business unit is part of P&G
+def validateBusinessUnit(businessUnit):
+    units = list(map(str.lower,['Baby Care', 'Fabric Care', 'Family Care', 'Feminine Care', 'Hair Care', 'Home Care', 'Oral Care', 'Personal Health Care', 'Shave Care', 'Skin and Personal Cleansing', 'Beauty Care', 'Global Business Services']))
+    # print(units)
+    businessUnit = str.lower(businessUnit)
+    # print(businessUnit)
+    for unit in units:
+        # print(unit)
+        if businessUnit == unit:
+            return str.title(unit)
+        else:
+            unit = 'Unknown'
+           
+    return unit
+
+# for tracing purposes
+def readBusinessUnit():
+    units = list(map(str.lower,['Baby Care', 'Fabric Care', 'Family Care', 'Feminine Care', 'Hair Care', 'Home Care', 'Oral Care', 'Personal Health Care', 'Shave Care', 'Skin and Personal Cleansing', 'Beauty Care', 'Global Business Services']))
+    # print(units)
+    # businessUnit = str.lower(businessUnit)
+    # print(businessUnit)
+    for unit in units:
+        print(unit)
+        # if businessUnit != unit:
+        #     return 'Unknown Business Unit'
+        # else:
+        #     businessUnit = str.upper(businessUnit)
+
 
 # def nullToBlank(f):
 #     if f == ''
--- testproject/testapp_1/views.py
+++ testproject/testapp_1/views.py
@@ -5,7 +5,7 @@ from .models import fileRead, testModel
 from .forms import UploadFileForm
 from .resources import FileReadResource
 from tablib import Dataset
-from .functions import validateEmail
+from .functions import validateEmail, validateBusinessUnit, readBusinessUnit
 
 def index(request):
     return render(request,'index.html')
@@ -47,18 +47,25 @@ def importFile(request):
             # messages.info(request,'Wrong File Format')
             return render(request,'import.html',{'note':'Wrong File Format'})
 
+        # for tracing purposes
+        # readBusinessUnit()
+
         imported_data = dataset.load(new_file.read(),format='xlsx')
+        # intial loop
+        # 
         # for data in imported_data:
         #     value = fileRead(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
         #                         data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20])
         #     value.save()
         for data in imported_data:
 
-            print(data[2])
+            # checks if the email is valid
             validatedEmail = validateEmail(data[2])
-            print(validatedEmail)
 
-            value = fileRead(data[0],data[1],validatedEmail,data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
+            # checks if the business unit is part of P&G
+            validatedBU = validateBusinessUnit(data[4])
+           
+            value = fileRead(data[0],data[1],validatedEmail,data[3],validatedBU,data[5],data[6],data[7],data[8],data[9],data[10],
                                 data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20])
             value.save()
         return render(request, 'import.html',{'note':'Imported'})
