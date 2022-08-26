1edf410d77d852a7ab797b6991e2b1f8f803a4b0
katemagbitang <breakatedown@gmail.com>
2022-08-26 18:16:19 +0800

    initial basic validation for complete excel import

--- testproject/testapp_1/views.py
+++ testproject/testapp_1/views.py
@@ -54,48 +54,48 @@ def importFile(request):
         imported_data = dataset.load(new_file.read(),format='xlsx')
         # intial loop
         # 
-        # for data in imported_data:
-        #     value = fileRead(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
-        #                         data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20])
-        #     value.save()
         for data in imported_data:
+            value = fileRead(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
+                                data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20])
+            value.save()
+        # for data in imported_data:
 
-            mandatory_fields = [data[4],data[5],data[6],data[11],data[12],data[13],data[14],data[15],data[16],data[20]]
+            # mandatory_fields = [data[4],data[5],data[6],data[11],data[12],data[13],data[14],data[15],data[16],data[20]]
             
             # check if the mandatory fields are empty to be replaced with another string
-            isFilledBU = replaceEmptyFields(data[4])
-            isFilledPlant = replaceEmptyFields(data[5])
-            isFilledReqName = replaceEmptyFields(data[6])
-            isFilledMatDes = replaceEmptyFields(data[11])
-            isFilledMeasureUnit = replaceEmptyFields(data[12])
-            isFilledMatGroup = replaceEmptyFields(data[13])
-            isFilledManuName = replaceEmptyFields(data[14])
-            isFilledPartNum = replaceEmptyFields(data[15])
-            isFilledAttach = replaceEmptyFields(data[16]) # url or file path soon
-            isFilledLocation = replaceEmptyFields(data[20])
+            # isFilledBU = replaceEmptyFields(data[4])
+            # isFilledPlant = replaceEmptyFields(data[5])
+            # isFilledReqName = replaceEmptyFields(data[6])
+            # isFilledMatDes = replaceEmptyFields(data[11])
+            # isFilledMeasureUnit = replaceEmptyFields(data[12])
+            # isFilledMatGroup = replaceEmptyFields(data[13])
+            # isFilledManuName = replaceEmptyFields(data[14])
+            # isFilledPartNum = replaceEmptyFields(data[15])
+            # isFilledAttach = replaceEmptyFields(data[16]) # url or file path soon
+            # isFilledLocation = replaceEmptyFields(data[20])
             
             # checks if the email is valid
-            validatedEmail = validateEmail(data[2])
+            # validatedEmail = validateEmail(data[2])
             # print(validatedEmail)
 
             # checks if the business unit is part of P&G
-            validatedBU = validateBusinessUnit(isFilledBU)
+            # validatedBU = validateBusinessUnit(isFilledBU)
             # print(validatedBU)
 
             # checks if the plant code and name is part of P&G
-            validatedPlant = validatePlant(isFilledPlant)
+            # validatedPlant = validatePlant(isFilledPlant)
             # print(validatedPlant)
 
             # checks if the unit of measurement is valid
-            validatedMeasureUnit = validateMeasureUnit(isFilledMeasureUnit)
+            # validatedMeasureUnit = validateMeasureUnit(isFilledMeasureUnit)
             # print(validatedPlant)
 
             # checks if the material group is valid
-            validatedMatGrp = validateMaterialGrp(isFilledMatGroup)
+            # validatedMatGrp = validateMaterialGrp(isFilledMatGroup)
         
-            value = fileRead(data[0],data[1],validatedEmail,data[3],validatedBU,validatedPlant,isFilledReqName,data[7],data[8],data[9],data[10],
-                                isFilledMatDes,validatedMeasureUnit,validatedMatGrp,isFilledManuName,isFilledPartNum,isFilledAttach,data[17],data[18],data[19],isFilledLocation)
-            value.save()
+            # value = fileRead(data[0],data[1],validatedEmail,data[3],validatedBU,validatedPlant,isFilledReqName,data[7],data[8],data[9],data[10],
+            #                     isFilledMatDes,validatedMeasureUnit,validatedMatGrp,isFilledManuName,isFilledPartNum,isFilledAttach,data[17],data[18],data[19],isFilledLocation)
+            # value.save()
         return render(request, 'import.html',{'note':'Imported'})
     else:
         return render(request, 'import.html')
@@ -121,43 +121,40 @@ def partOneImportFile(request):
 
             if (isComplete):
 
+                # checks if the unit of measurement is valid
+                validatedMeasureUnit = validateMeasureUnit(data[4])
+                # print(validatedPlant)
 
-            # isFilledMatDes = replaceEmptyFields(data[3])
-            # isFilledMeasureUnit = replaceEmptyFields(data[4])
-            # isFilledMatGroup = replaceEmptyFields(data[5])
-            # isFilledManuName = replaceEmptyFields(data[6])
-            # isFilledPartNum = replaceEmptyFields(data[7])
-            # isFilledAttach = replaceEmptyFields(data[8]) # url or file path soon
-            # isFilledLocation = replaceEmptyFields(data[12])
-            # isFilledSecurity = replaceEmptyFields(data[17])
+                # checks if the material group is valid
+                validatedMatGrp = validateMaterialGrp(data[5])
 
-            # checks if the unit of measurement is valid
-            # validatedMeasureUnit = validateMeasureUnit(isFilledMeasureUnit)
-            # print(validatedPlant)
+                #checks if the security classification is valid
+                validatedSecurity = validateSecurity(data[17])
 
-            # checks if the material group is valid
-            # validatedMatGrp = validateMaterialGrp(isFilledMatGroup)
+                value = partOneImport(data[0],data[1],data[2],data[3],validatedMeasureUnit,validatedMatGrp,data[6],data[7],data[8],data[9],data[10],
+                                    data[11],data[12],data[13],data[14],data[15],data[16],validatedSecurity,data[18])
 
-            #checks if the security classification is valid
-            # validatedSecurity = validateSecurity(isFilledSecurity)
-
-            # value = partOneImport(data[0],data[1],data[2],isFilledMatDes,validatedMeasureUnit,validatedMatGrp,isFilledManuName,isFilledPartNum,isFilledAttach,data[9],data[10],
-            #                     data[11],isFilledLocation,data[13],data[14],data[15],data[16],validatedSecurity,data[18])
-
-                value = partOneImport(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
-                                data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18])
+                # value = partOneImport(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
+                #                 data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18])
 
                 value.save()
             else:
-                newValues = []
-                newValues = replaceEmptyFields(data,len(data))
+                # replace strings function
+                #
+                # newValues = []
+                # newValues = replaceEmptyFields(data,len(data))
 
-                value = partOneImport(data[0],newValues[1],newValues[2],newValues[3],newValues[4],newValues[5],newValues[6],newValues[7],newValues[8],newValues[9],newValues[10],
-                                newValues[11],newValues[12],newValues[13],newValues[14],newValues[15],newValues[16],newValues[17],newValues[18])
+                # value = partOneImport(data[0],newValues[1],newValues[2],newValues[3],newValues[4],newValues[5],newValues[6],newValues[7],newValues[8],newValues[9],newValues[10],
+                #                 newValues[11],newValues[12],newValues[13],newValues[14],newValues[15],newValues[16],newValues[17],newValues[18])
 
-                value.save()
+                # value = partOneImport(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],
+                #                 data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18])
 
-                return render(request,'partOneImport.html',{'note':'Replaced fields'})
+                # value.save()
+
+                # return render(request,'partOneImport.html',{'note':'Replaced fields'})
+
+                return render(request,'partOneImport.html',{'note':'Incomplete fields'})
         
         return render(request, 'partOneImport.html',{'note':'Imported'})
     else:
